0695a890636c8b249c7b4fa6ac91808d05884b86
maradondt <maradondt@gmail.com>
2021-02-08 13:48:49 +0300

    Вернул proxyUrl

--- src/init.js
+++ src/init.js
@@ -26,12 +26,11 @@ const createPosts = (posts, feedId) => posts
   }));
 
 const getRSS = (url) => axios
-  // .get(`https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${url}`) Proxy url
-  .get(url)
+  .get(`https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${url}`)
+  // .get(url)
   .then((responce) => responce.data)
-  .catch((err) => {
-    state.networkErrors = ['networkUpdateIssue'];
-    throw new Error(err);
+  .catch(() => {
+    throw new Error('networkUpdateIssue');
   });
 
 const initModal = () => {
@@ -77,7 +76,6 @@ const autoupdate = (feedState) => {
       .catch((err) => {
         upState.errors = [err.message];
         console.warn(err);
-        console.log(state);
       });
   });
   setTimeout(autoupdate, delayInSeconds * 1000, upState);
